93957eec66f7699bb18c76ad319629ca9503b7ca
Bhargav <bhargavpatel6040@gmail.com>
2020-12-29 23:04:47 -0500

    Adding menu component using reactstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristornate de Confusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
